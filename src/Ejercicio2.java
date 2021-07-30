ff63ee2d0c80b160e6030e2fe4e89e5d8104a15d
carcaceres <car.caceres.ochoa@hotmail.com>
2021-07-30 11:30:38 -0500

    sesion1 modulo java g28 sa

---
+++ src/Ejercicio2.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DELL
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         // TODO code application logic here
+        // comentario simple
+        /**
+          hola esto es un comentario 
+          de mas de una linea
+         */
+        int x=4; // tipo de dato entero
+        double d=45.5; // tipo de dato decimal
+        char c='c'; // tipo de dato char
+        String cadena="hola a todos"; // tipo de cadena
+        boolean b1=false; // tipo de dato boolean
+        boolean b2=true; // tipo de datos boolean
+        System.out.println("x="+x);
+        System.out.println("d="+d);
+        System.out.println("c="+c);
+        System.out.println("cadena"+cadena);
+        System.out.println("b1="+b1);
+        System.out.println("b2="+b2);
+        System.out.println("2"+(3+2));
+        
+        
+    }
+    
+}
